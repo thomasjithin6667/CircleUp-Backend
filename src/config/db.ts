9ace11029958ddcd8d710671e1252e67b29ab49b
thomasjithin6667 <thomasjohn6667@gmail.com>
2024-04-25 20:31:36 +0530

    hosting

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
   try {
-    const mongoURI: any = process.env.MONGO_URL;
+     ;
 
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(process.env.MONGO_URL as string);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
